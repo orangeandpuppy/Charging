3b208c79db94a396a28d09e229c63f87b3c1dc93
zzy-yuki <93981237+zzy-yuki@users.noreply.github.com>
2024-05-28 19:21:53 +0800

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-from apps import create_app
-from config import FLASK_HOST, FLASK_PORT, FLASK_DEBUG
-
-
-if __name__ == '__main__':
-    # 创建应用实例
-    app = create_app()
-    # 启动应用
-    app.run(host=FLASK_HOST, port=FLASK_PORT, debug=FLASK_DEBUG)
